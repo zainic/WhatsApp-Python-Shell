1e3a06785d81cff2bd160151b72b1bebdc824a78
Dzaky Tamir <dzakytamir@student.ub.ac.id>
2022-06-28 21:31:43 +0700

    Build open_dir command and create function for check new msg

--- main.py
+++ main.py
@@ -44,6 +44,26 @@ def run(code):
         print(type(e))
     return output
 
+# Check new message
+def check_new_message(driver, chat_id, include_me=True, exit=False):
+    while True:
+        new_messages = driver.get_unread_messages_in_chat(chat_id, include_me)
+        if not driver.is_connected():
+            print("Connecting To Server")
+            if detect(2):
+                exit = True
+                break
+            continue
+        if len(new_messages) == 0:
+            print("No messages Detected")
+            if detect(3):
+                exit = True
+                break
+        else:
+            print("Message Detected")
+            break
+    return exit, new_messages
+
 LIST_COMMANDS = []
 
 def login():
@@ -76,25 +96,10 @@ def main(only_me = True, chat_id = "628568002060@c.us"):
             continue
         
         # Check if there are new message
-        while True:
-            new_messages = driver.get_unread_messages_in_chat(chat_id, include_me=True)
-            if not driver.is_connected():
-                print("Connecting To Server")
-                if detect(2):
-                    exit = True
-                    break
-                continue
-            if len(new_messages) == 0:
-                print("No messages Detected")
-                if detect(3):
-                    exit = True
-                    break
-            else:
-                print("Message Detected")
-                break
+        exit, new_messages = check_new_message(driver, chat_id)
         
         # Get command message and run it
-        for message in new_messages[-1:]:
+        for message in new_messages:
             sender_id = message.sender.id
             # Check the command keyword
             if message.type in ['image','video','sticker']:
@@ -203,10 +208,86 @@ def main(only_me = True, chat_id = "628568002060@c.us"):
                 LIST_COMMANDS.append(docs)
                 print(f"command {name_function} with parameters {params} was created")
             
-            
-            
-            
-        
-            
-            
-    
+            if first_line[0] == '\\open_dir':
+                """
+                This function allows us to explore our PC's folder using WhatsApp
+                It will disable other function while you use it except run command
+                
+                Ex:
+                '\open_dir file_path'
+                    
+                (without using '' and the default file_path is current github directory as ".")
+                
+                To navigate your path you can use cd function like bash
+                '\cd name_folder' 
+                note : if file doesn't exist it won't move
+                
+                To exit the explorer type this command
+                '\quit'
+                
+                """
+                try:
+                    directory = os.path.join(first_line[1])
+                except:
+                    directory = os.path.join('.')
+                show = True
+                quit = False 
+                msg = message
+                while not quit:
+                    if show:
+                        old_stdout = sys.stdout
+                        new_stdout = io.StringIO()
+                        sys.stdout = new_stdout
+                        try:
+                            for i in os.listdir(directory):
+                                print(i)
+                        except:
+                            print('failed to open directory')
+                            directory = temp_dir
+                        sys.stdout = old_stdout
+                        output = new_stdout.getvalue()
+                        msg.reply_message(str(output))
+                        show = False
+                    
+                    # Check local new message
+                    quit, messages = check_new_message(driver, chat_id, exit = quit)
+                    
+                    for msg in messages:
+                        sender_id2 = msg.sender.id
+                        # Check the command keyword
+                        if msg.type in ['image','video','sticker']:
+                            continue
+                        if msg.content[0] == "\\":
+                            if only_me == True and sender_id2 != "628568002060@c.us":
+                                print("This user can't use the bot")
+                                continue
+                            cmd = msg.content.split('\n')
+                            first = cmd[0].split()
+                        else:
+                            continue
+                        
+                        if first[0] == '\\quit':
+                            """
+                            Exit the virtual explorer
+                            """
+                            quit = True
+                            break
+                        
+                        if first[0] == '\\run':
+                            """
+                            Run python command
+                            """
+                            code = "\n".join(cmd[1:])
+                            output = run(code)
+                            msg.reply_message(str(output))
+                        
+                        if first[0] == '\\cd':
+                            """
+                            Open the folder or navigate the explorer
+                            """
+                            show = True
+                            temp_dir = directory
+                            directory = os.path.join(directory, first[1])
+                
+                print("Quitting the explorer")
+                msg.reply_message("Quitting the explorer")
