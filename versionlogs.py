23d30e31a04e52f36496594404b739222971c479
Dzaky Tamir <dzakytamir@student.ub.ac.id>
2022-07-01 18:40:59 +0700

    Updating version logs, adding v1.1 logs

--- versionlogs.py
+++ versionlogs.py
@@ -1,6 +1,9 @@
 logs = {
     '1.0' : {
         'Description' : "Create python shell in WhatsApp. It's allow us to connect the phone and PC using WhatsApp. We can take any file from our PC without even we touch it again. The full tutorial and how to setup the bot is on my github https://github.com/zainic/WhatsApp-Python-Shell",
+        'Changes' : [
+            "It has no changes yet"
+        ],
         'Added Commands' : {
             'help' : 'Show list of command and its utility.',
             'home' : 'Place or move the bot to user id or operator id',
@@ -24,6 +27,29 @@ logs = {
             'The open_dir command output is not align.',
             'The message sometimes detected twice, so the output also twice.',
             'The runs command sometimes failed to send the message.',
+        ],
+    },
+    '1.1' : {
+        'Description' : "Hey there, Now the bot can be placed in multiple chat room or groups. This update mostly for security things and operator only command. Also now you can see the version log by using \\version command to see how the program was developed. There is also some bug fixes from previous version",
+        'Changes' : [
+            "The command home, exit, change, ban, and whitelist now only can be used by operator.",
+            "Change chat_id variable to list since it now can be more than one place.",
+            "Create version logs",
+        ],
+        'Added Commands' : {
+            'version' : 'Show list of version and its change logs.',
+            'duplicate' : 'Duplicate the place of bot.',
+            'remove' : 'Show list of version and its change logs.',
+            'toggle_only_me' : 'Toggle the only me variable for using the bot.',
+            'version' : 'Show list of version and its change logs.',
+        },
+        'Fixed Bugs' : [
+            'The open_dir command output is now aligned.',
+            'The message no longer sometimes detected twice.'
+        ],
+        'Known bugs' : [
+            'The runs command sometimes failed to send the message.',
+            'When sending files from PC often to be failed.',
         ],
     },
 }
